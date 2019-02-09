3484377d70cd5e92844c531ba473ec9ed782f739
Kevin Lee <klee8880@gmail.com>
2019-02-09 02:33:33 -0600

    Added testing & removed example code

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,33 +21,6 @@ public class Main {
         IJPaintController controller = new JPaintController(uiModule, appState, Manager);
         controller.setup();
         paintCanvas.addMouseListener(new MouseHandler(appState, paintCanvas, Manager));
-        
-        // For example purposes only; remove all lines below from your final project.
-
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-        // Filled in rectangle
-        Graphics2D graphics2d = paintCanvas.getGraphics2D();
-        graphics2d.setColor(Color.GREEN);
-        graphics2d.fillRect(12, 13, 200, 400);
-
-        // Outlined rectangle
-        graphics2d.setStroke(new BasicStroke(5));
-        graphics2d.setColor(Color.BLUE);
-        graphics2d.drawRect(12, 13, 200, 400);
-
-        // Selected Shape
-        Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
-        graphics2d.setStroke(stroke);
-        graphics2d.setColor(Color.BLACK);
-        graphics2d.drawRect(7, 8, 210, 410);
-
-        // Clears the Canvas
-        paintCanvas.paintImmediately(0, 0, paintCanvas.getWidth(), paintCanvas.getHeight());
     }
 }
 
--- src/model/Shapes/TESTShapes.java
+++ src/model/Shapes/TESTShapes.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import model.Point;
+import model.interfaces.Entity;
 import model.persistence.ApplicationState;
 import view.gui.Gui;
 import view.gui.GuiWindow;
@@ -21,16 +22,108 @@ public class TESTShapes {
     IUiModule uiModule = new Gui(guiWindow);
     ApplicationState appState = new ApplicationState(uiModule);
 
+    @Test
+	public void moveTriangle() {
+		Entity shape1 = ShapeFactory.makeTriangle(appState, new Point(4,4), new Point(10,10));
+		Entity shape2 = ShapeFactory.makeTriangle(appState, new Point(4,4), new Point(10,10));
+		Entity shape3 = ShapeFactory.makeTriangle(appState, new Point(4,4), new Point(10,10));
+		Entity shape4 = ShapeFactory.makeTriangle(appState, new Point(4,4), new Point(10,10));
+		
+		shape1.move(-3, 0);
+		shape2.move(3, 0);
+		shape3.move(0, -3);
+		shape4.move(0, 3);
+		
+		//start points change
+		assertEquals("Negative x movement failed",1,shape1.getStartPoint().x);
+		assertEquals("Positive x movement failed",7,shape2.getStartPoint().x);
+		assertEquals("Negative y movement failed",1,shape3.getStartPoint().y);
+		assertEquals("Positive y movement failed",7,shape4.getStartPoint().y);
+		
+		//Dimensions do not change
+		assertEquals("Height altered",6,shape1.getHeight());
+		assertEquals("Height altered",6,shape2.getHeight());
+		assertEquals("Height altered",6,shape3.getHeight());
+		assertEquals("Height altered",6,shape4.getHeight());
+		
+		assertEquals("Width altered",6,shape1.getWidth());
+		assertEquals("Width altered",6,shape2.getWidth());
+		assertEquals("Width altered",6,shape3.getWidth());
+		assertEquals("Width altered",6,shape4.getWidth());
+		
+	}
+    
+    @Test
+	public void moveRectangle() {
+		Entity shape1 = ShapeFactory.makeRectangle(appState, new Point(4,4), new Point(10,10));
+		Entity shape2 = ShapeFactory.makeRectangle(appState, new Point(4,4), new Point(10,10));
+		Entity shape3 = ShapeFactory.makeRectangle(appState, new Point(4,4), new Point(10,10));
+		Entity shape4 = ShapeFactory.makeRectangle(appState, new Point(4,4), new Point(10,10));
+		
+		shape1.move(-3, 0);
+		shape2.move(3, 0);
+		shape3.move(0, -3);
+		shape4.move(0, 3);
+		
+		//start points change
+		assertEquals("Negative x movement failed",1,shape1.getStartPoint().x);
+		assertEquals("Positive x movement failed",7,shape2.getStartPoint().x);
+		assertEquals("Negative y movement failed",1,shape3.getStartPoint().y);
+		assertEquals("Positive y movement failed",7,shape4.getStartPoint().y);
+		
+		//Dimensions do not change
+		assertEquals("Height altered",6,shape1.getHeight());
+		assertEquals("Height altered",6,shape2.getHeight());
+		assertEquals("Height altered",6,shape3.getHeight());
+		assertEquals("Height altered",6,shape4.getHeight());
+		
+		assertEquals("Width altered",6,shape1.getWidth());
+		assertEquals("Width altered",6,shape2.getWidth());
+		assertEquals("Width altered",6,shape3.getWidth());
+		assertEquals("Width altered",6,shape4.getWidth());
+	}
+    
+    @Test
+	public void moveOval() {
+		Entity shape1 = ShapeFactory.makeEllipse(appState, new Point(4,4), new Point(10,10));
+		Entity shape2 = ShapeFactory.makeEllipse(appState, new Point(4,4), new Point(10,10));
+		Entity shape3 = ShapeFactory.makeEllipse(appState, new Point(4,4), new Point(10,10));
+		Entity shape4 = ShapeFactory.makeEllipse(appState, new Point(4,4), new Point(10,10));
+		
+		shape1.move(-3, 0);
+		shape2.move(3, 0);
+		shape3.move(0, -3);
+		shape4.move(0, 3);
+		
+		//start points change
+		assertEquals("Negative x movement failed",1,shape1.getStartPoint().x);
+		assertEquals("Positive x movement failed",7,shape2.getStartPoint().x);
+		assertEquals("Negative y movement failed",1,shape3.getStartPoint().y);
+		assertEquals("Positive y movement failed",7,shape4.getStartPoint().y);
+		
+		//Dimensions do not change
+		assertEquals("Height altered",6,shape1.getHeight());
+		assertEquals("Height altered",6,shape2.getHeight());
+		assertEquals("Height altered",6,shape3.getHeight());
+		assertEquals("Height altered",6,shape4.getHeight());
+		
+		assertEquals("Width altered",6,shape1.getWidth());
+		assertEquals("Width altered",6,shape2.getWidth());
+		assertEquals("Width altered",6,shape3.getWidth());
+		assertEquals("Width altered",6,shape4.getWidth());
+	}
+    
 	@Test
 	public void triangleTest(){
-		int [] xArray = {1,1,7};
-		int [] yArray = {1,7,7};
 		
 		ETriangle shape = (ETriangle) ShapeFactory.makeTriangle(appState, new Point(1,1), new Point(7,7));
 		
 		assertEquals("Unexpected Height",6 ,shape.getHeight());
 		assertEquals("Unexpected Width",6 ,shape.getWidth());
 		assertTrue("Unexpeced Starting Point", new Point(1,1).equals(shape.getStartPoint()));
+		
+		int [] xArray = {1,1,7};
+		int [] yArray = {1,7,7};
 		
 		for (int i = 0; i < 3; i++) {
 			assertEquals("Unexpect X point [" + i + "]in triangle", xArray[i], shape.getXArray()[i]);
@@ -40,18 +133,40 @@ public class TESTShapes {
 	
 	@Test
 	public void rectangeTest() {
-		fail("Not implimented");
+		
+		ERectangle shape = (ERectangle) ShapeFactory.makeRectangle(appState, new Point(1,1), new Point(7,7));
+		
+		assertEquals("Unexpected Height",6 ,shape.getHeight());
+		assertEquals("Unexpected Width",6 ,shape.getWidth());
+		assertTrue("Unexpeced Starting Point", new Point(1,1).equals(shape.getStartPoint()));
 		
 	}
 	
 	@Test
 	public void ovalTest() {
-		fail("Not implimented");
+		
+		EEllipse shape = (EEllipse) ShapeFactory.makeEllipse(appState, new Point(1,1), new Point(7,7));
+		
+		assertEquals("Unexpected Height",6 ,shape.getHeight());
+		assertEquals("Unexpected Width",6 ,shape.getWidth());
+		assertTrue("Unexpeced Starting Point", new Point(1,1).equals(shape.getStartPoint()));
 	}
 	
 	@Test
 	public void shapeFactoryFunctional() {
-		fail("Not implimented");
+		
+		Entity shape1 = null;
+		Entity shape2 = null;
+		Entity shape3 = null;
+
+		shape1 = ShapeFactory.makeTriangle(appState,  new Point(1,1), new Point(7,7));
+		shape2 = ShapeFactory.makeRectangle(appState,  new Point(1,1), new Point(7,7));
+		shape3 = ShapeFactory.makeEllipse(appState,  new Point(1,1), new Point(7,7));
+		
+		if (shape1 == null) fail("No Triangle created");
+		if (shape2 == null) fail("No Rectangle created");
+		if (shape3 == null) fail("No Ellipse created");
+		
 	}
 	
 }
